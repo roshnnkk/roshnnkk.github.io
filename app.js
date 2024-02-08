b71e87db2912170b9b8b6133ead60c66883b4c58
roshnnkk <roshanakabd78@gmail.com>
2024-02-08 19:54:59 +0330

    correcting js on hamburger button

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
 const menuButton = document.getElementById("hamburger-btn");
 const showMenu = document.getElementById("hamburger-show");
 
-function openHamburgerMenu() {
+function removeMenu() {
+  showMenu.style.display = "none";
+  menuButton.classList.add("hamburger-menu");
+  menuButton.classList.remove("change-button");
+}
+
+function addMenu() {
+  showMenu.style.display = "block";
   menuButton.classList.remove("hamburger-menu");
   menuButton.classList.add("change-button");
-  showMenu.style.display = "block";
+}
 
-//   const backButton = document.getElementsByClassName("change-button");
-
-//   function closeHamburgerMenu() {
-//     backButton.onclick = function () {
-
-//       menuButton.classList.remove("change-button");
-
-
-//     };
-//   }
+function openHamburgerMenu() {
+  var showMenu = document.getElementById("hamburger-show");
+  if (showMenu.style.display === "block") {
+    removeMenu();
+  } else {
+    addMenu();
+  }
 }
