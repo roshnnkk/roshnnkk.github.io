1546fe053673fc35dbff650f712d81cedbcc4149
roshnnkk <roshanakabd78@gmail.com>
2024-02-09 14:01:49 +0330

    add js on my work slider

--- app.js
+++ app.js
@@ -21,3 +21,53 @@ function openHamburgerMenu() {
     addMenu();
   }
 }
+
+///////////////my work slider
+
+const images = [
+  {
+    img: (src = "assets/mywork1.png"),
+  },
+
+  {
+    img: (src = "assets/mywork2.png"),
+  },
+
+  {
+    img: (src = "assets/mywork3.png"),
+  },
+
+  {
+    img: (src = "assets/mywork4.png"),
+  },
+];
+
+const newImg = document.getElementById("my-work");
+document.addEventListener("DOMContentLoaded", function () {
+  const imgOne = images[0];
+  newImg.src = imgOne.img;
+});
+///// button click actions
+
+let currentImg = 0;
+
+document.getElementById("next-btn").addEventListener("click", function () {
+  if ((currentImg >= 0, currentImg < images.length - 1)) {
+    currentImg++;
+    newImg.src = images[currentImg].img;
+  } else if (currentImg >= images.length - 1) currentImg = 0;
+  newImg.src = images[currentImg].img;
+});
+
+document.getElementById("back-btn").addEventListener("click", function () {
+  if (currentImg > 0) {
+    currentImg--;
+    newImg.src = images[currentImg].img;
+  }else{
+    currentImg = images.length - 1;
+    newImg.src = images[currentImg].img;
+  }
+
+
+
+});
